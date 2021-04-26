b4f6a3c5249962d6301aeae8f94dc5292e6d270f
JevgeniTIG <jastream@hotmail.com>
2021-04-26 22:35:34 +0300

    mail notification implementation v1.3

--- src/main/java/com/example/stocks/config/Conf.java
+++
@@ -1,9 +0,0 @@
-package com.example.stocks.config;
-
-public class Conf {
-	public final static String MAIL_TO = "jastream@hotmail.com";
-	public final static String MAIL_FROM = "stockradar.ee@hotmail.com";
-	public final static String MAIL_USER = "stockradar.ee@hotmail.com";
-	public final static String MAIL_PASS = "High71ght3D@";
-	public final static String MAIL_HOST = "smtp-mail.outlook.com";
-}
