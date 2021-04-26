156649ce5a1a738007e8b5cddc944f94e2124d36
JevgeniTIG <jastream@hotmail.com>
2021-04-26 21:53:17 +0300

    mail notification implementation v1.1

--- src/main/java/com/example/stocks/web/TestController.java
+++ src/main/java/com/example/stocks/web/TestController.java
@@ -1,11 +1,9 @@
 package com.example.stocks.web;
 
-import com.example.stocks.service.GetStockWikiDataService;
 import com.example.stocks.service.TestingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.math.BigDecimal;
 
 @RestController
@@ -14,15 +12,8 @@ import java.math.BigDecimal;
 public class TestController {
 
 	@Autowired
-	private GetStockWikiDataService getStockWikiDataService;
-	@Autowired
 	private TestingService testingService;
 
-
-	@PostMapping("/test-wiki-data")
-	public String getStockWikiData(@RequestParam(value="ticker") String ticker) throws Exception {
-		return getStockWikiDataService.makeSearch(ticker);
-	}
 
 	@PostMapping("/create-hs")
 	public void createHS(@RequestParam("ticker") String ticker,
