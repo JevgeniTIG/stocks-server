099a54b2429a2785c391ac4a4dfa539b43318bd3
JevgeniTIG <jastream@hotmail.com>
2021-05-26 09:16:47 +0300

    price service changed

--- src/main/java/com/example/stocks/service/ScheduledExecutorService.java
+++ src/main/java/com/example/stocks/service/ScheduledExecutorService.java
@@ -1,39 +1,39 @@
-//package com.example.stocks.service;
-//
-//import com.example.stocks.entity.HighlightedStock;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.scheduling.annotation.EnableScheduling;
-//import org.springframework.scheduling.annotation.Scheduled;
-//
-//import java.util.List;
-//
-//@Configuration
-//@EnableScheduling
-//public class ScheduledExecutorService {
-//
-//	private PriceService priceService;
-//	private MailNotificationService mailNotificationService;
-//
-//	public ScheduledExecutorService(PriceService priceService,
-//									MailNotificationService mailNotificationService){
-//		this.priceService = priceService;
-//		this.mailNotificationService = mailNotificationService;
-//	}
-//
-//
-//
-//	@Scheduled(cron = "0 0 6 * * TUE,WED,THU,FRI,SAT")
-//	public void saveStocksPricesAsScheduled(){
-//
-//		priceService.savePriceDaily();
-//
-//		List<HighlightedStock> highlightedStocksList = priceService.evaluateStockPrices();
-//		if (highlightedStocksList.size() > 0) {
-//			mailNotificationService.sendMail();
-//		}
-//	}
-//
-//
-//
-//
-//}
+package com.example.stocks.service;
+
+import com.example.stocks.entity.HighlightedStock;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.List;
+
+@Configuration
+@EnableScheduling
+public class ScheduledExecutorService {
+
+	private PriceService priceService;
+	private MailNotificationService mailNotificationService;
+
+	public ScheduledExecutorService(PriceService priceService,
+									MailNotificationService mailNotificationService){
+		this.priceService = priceService;
+		this.mailNotificationService = mailNotificationService;
+	}
+
+
+
+	@Scheduled(cron = "0 0 6 * * TUE,WED,THU,FRI,SAT")
+	public void saveStocksPricesAsScheduled(){
+
+		priceService.savePriceDaily();
+
+		List<HighlightedStock> highlightedStocksList = priceService.evaluateStockPrices();
+		if (highlightedStocksList.size() > 0) {
+			mailNotificationService.sendMail();
+		}
+	}
+
+
+
+
+}
